401adc1d36314029a383cb1fed7e7ad8329b83a9
Moses <moses.oyelade@student.moringaschool.com>
2023-07-28 22:28:35 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+//moving dodger-left
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  console.log(moveDodgerLeft);
+
+//moving dodger-right
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left= parseInt(leftNumbers, 10);
+
+    if (left < 400) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+console.log(moveDodgerRight);
+
+//Wire to event listener
+dodger.style.bottom = "10px";
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+ 
